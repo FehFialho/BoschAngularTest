95de16efc7adb1a47baed90932e784a37f81257d
FehFialho <fernandaklechowicz@gmail.com>
2026-04-17 14:19:03 -0300

    feat: implemented input

--- uatzap/src/app/features/contact-page/contact-page.ts
+++ uatzap/src/app/features/contact-page/contact-page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Button } from '../../shared/button/button';
-import { Input } from '../../shared/input/input';
+import { MyInput } from '../../shared/input/input';
 import { Header } from '../../shared/header/header';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [Button, Input, Header],
+  imports: [Button, MyInput, Header],
   templateUrl: './contact-page.html',
   styleUrl: './contact-page.css',
 })
--- uatzap/src/app/shared/input/input.ts
+++ uatzap/src/app/shared/input/input.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.html',
   styleUrl: './input.css',
 })
-export class Input {}
+export class MyInput {
+
+  @Input()
+  Placeholder: string = "";
+  @Output()
+  onChange: EventEmitter<string> = new EventEmitter();
+  value = this.Placeholder; // Recebe o mesmo valor inicial de PlaceHolder
+
+  onInputChange = (value: any) => {
+    const insertedValue = value.srcElement?.value;
+    this.onChange.emit(insertedValue)
+    console.log('insertedValue: ', insertedValue);
+    console.log('value: ', value);
+  }
+}
+
