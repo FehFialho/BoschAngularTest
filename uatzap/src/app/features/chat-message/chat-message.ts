be72dfcb0f9e821a6a2784fb5fa420c91d55ba81
FehFialho <fernandaklechowicz@gmail.com>
2026-04-17 13:37:20 -0300

    feat: updated header

--- uatzap/src/app/features/chat-message/chat-message.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-chat-message',
-  imports: [],
-  templateUrl: './chat-message.html',
-  styleUrl: './chat-message.css',
-})
-export class ChatMessage {}
