7c25dd38c7873051f9f4ae474d4a4dd6966b5be4
FehFialho <fernandaklechowicz@gmail.com>
2026-04-17 13:31:22 -0300

    feat: added basic routes

--- uatzap/src/app/app.routes.ts
+++ uatzap/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AuthPage } from './features/auth-page/auth-page';
+import { LoginPage } from './features/auth-page/login-page/login-page';
+import { RegisterPage } from './features/auth-page/register-page/register-page';
+import { NotFoundPage } from './features/not-found-page/not-found-page';
+import { ContactPage } from './features/contact-page/contact-page';
+import { ChatPage } from './features/chat-page/chat-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "auth", component:AuthPage, children: [
+    {path: "login", component:LoginPage},
+    {path: "register", component:RegisterPage}
+  ]},
+  {path: "", component:ContactPage},
+  {path: "chat", component:ChatPage},
+  {path: "**", component:NotFoundPage}
+];
